f24055bb92eec73e9fe5cc1909d4bc690a936902
Chanuli-Sandanayake <chanulisandanayake8@gmail.com>
2025-06-05 23:51:54 +0530

    splash screen created

--- app/src/main/java/com/example/voicepoint/MainActivity.java
+++ app/src/main/java/com/example/voicepoint/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.voicepoint;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 3000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(() -> {
+            // TODO: Replace with your next activity
+            //startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            finish();
+        }, SPLASH_DURATION);
     }
 }
